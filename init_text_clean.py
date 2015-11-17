f689132f6e5c8fc8ad8706228c8efd9a14fb21a2
yibinxu <xuyibin@gmail.com>
2015-11-17 15:29:42 -0600

    add del_long string function

---
+++ init_text_clean.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 16 16:18:23 2015
+
+@author: Yibin
+"""
+import re
+
+def del_long(string, maxlen):
+    """
+    Parameters
+    ----------
+    string : single page caption list
+    maxlen
+        
+    Returns
+    -------
+    cleaned and seperated SinglePageCap
+
+    """
+    if len(string) > maxlen:
+        return 
+    else:
+        return string
+        
+   
+def seperate_caption(SinglePageCap):
+    """
+    initial cleaning for single page caption:
+    1. remove leading and trailing white space
+    2. remove empty and long strings
+    3. Seperate one capition to several if contains ';'
+    
+    Parameters
+    ----------
+    SinglePageCap : single page caption list
+        
+    Returns
+    -------
+    cleaned and seperated SinglePageCap
+
+    """
+    for i,s in enumerate(SinglePageCap):
+        s=del_long(s, 250)
+        if len(s)>0:
+            s=s.strip()
+            parts = re.split(';', s)
+            if len(parts) <= 1:
+                SinglePageCap[i]  = s
+            else:
+                SinglePageCap[i]  = parts[0]
+                for extra in parts[1:]:
+                    SinglePageCap.append(extra)
+    return [ls for ls in SinglePageCap if len(s)>0]
+    
+#example
+    
+#test=["   one; TwO 3.4 5,6; seven.eight; nine,ten   ",
+#      "   ONE; two22; Hello; HOHO   ",
+#      "yOu;  ; Hello; HOHO   "]
+#test_new=seperate_caption(test)
