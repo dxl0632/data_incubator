16e37f110a55cadffe8fc384fd1d5798450e12f4
C1BingyiYang <bingyi_yang@yahoo.com>
2015-11-18 16:05:54 -0600

    Append last name to single first names

--- Bingyi Explore.py
+++ Bingyi Explore.py
@@ -2,9 +2,10 @@
 """
 Created on Wed Nov 18 11:27:19 2015
 
-@author: pc
+@author: Bingyi
 """
 
+"""
 import re
 
 str ="A, B, C, and D Smith"
@@ -15,7 +16,8 @@ def name_split(str):
         
         lastname=str2.rsplit(None, 1)[-1]
         #cut the last word
-        str2.rsplit(' ', 1)[0]
+        str2=str2.rsplit(' ', 1)[0]
+        print "str2={%s}" % str2
         
         namelist1=str2.split(',')        
 
@@ -29,4 +31,45 @@ namelist
 
 namelist=name_split("bingyi yang")
 namelist
+"""
+
+inputNameList=["A", "B", "C D", "E", "F", "G H", "I", "J K"]
+
+# output: ['A D', 'B D', 'C D', 'E H', 'F H', 'G H', 'I K', 'J K']
+
+def AppendLastName(inputNameList):
+    numWordsList=[x.strip().count(' ')+1 for x in inputNameList]
+    print numWordsList
+    
+    currIndividual=0
+    LastNames=[]
+    for x  in numWordsList:
+        if x>1:
+            LastNames += inputNameList[currIndividual].split()[1]
+        currIndividual+=1
+    #print LastNames      
+    
+    currIndividual=0
+    outputNameList=[]
+    for x  in numWordsList:
+        if x==1:
+            thisName=inputNameList[currIndividual]+" "+LastNames[0]
+        else:
+            thisName=inputNameList[currIndividual]
+            del LastNames[0]
+        #print thisName
+        outputNameList+=[thisName]
+        currIndividual+=1
+    return outputNameList      
+
+print AppendLastName(inputNameList)
+
+        
+        
+        
+        
+    
+
+
+
     
